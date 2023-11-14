6eef9433afaf4f1e5b5d17e2fe95d24a001fc6df
pdellisola <150759224+pdellisola@users.noreply.github.com>
2023-11-14 20:48:38 -0300

    Update craajhead.js

--- craajhead.js
+++ craajhead.js
@@ -1,33 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Hide the menu item if the user is logged in
     var menuItemToHide = document.querySelector('span.dropdown__item a[href="https://www.craaj.com/registration-form"]');
     var userAvatar = document.querySelector('.user__avatar');
     if (userAvatar && menuItemToHide) {
         menuItemToHide.parentElement.style.display = 'none';
     }
 
-    // Initialize the forms based on the toggle state
-    var coachToggle = document.getElementById('coachToggle');
     var coachForm = document.getElementById('block-1699916884056');
     var nonCoachForm = document.getElementById('block-1699916941256');
+    var yesCoachBtn = document.getElementById('yesCoach');
+    var noCoachBtn = document.getElementById('noCoach');
 
-    // Set initial visibility of forms (non-coach form visible, coach form hidden)
-    coachForm.style.display = 'none'; // Hide coach form
-    nonCoachForm.style.display = 'block'; // Show non-coach form
+    // Hide both forms initially
+    coachForm.style.display = 'none';
+    nonCoachForm.style.display = 'none';
 
-    // Add event listener to the toggle switch
-    coachToggle.addEventListener('change', function() {
-        if (this.checked) {
-            // If toggle is set to 'I'm a coach'
-            coachForm.style.display = 'block'; // Show coach form
-            nonCoachForm.style.display = 'none'; // Hide non-coach form
-        } else {
-            // If toggle is set to 'I'm not a coach'
-            coachForm.style.display = 'none'; // Hide coach form
-            nonCoachForm.style.display = 'block'; // Show non-coach form
-        }
+    // Event listeners for buttons
+    yesCoachBtn.addEventListener('click', function() {
+        coachForm.style.display = 'block';
+        nonCoachForm.style.display = 'none';
+    });
+
+    noCoachBtn.addEventListener('click', function() {
+        coachForm.style.display = 'none';
+        nonCoachForm.style.display = 'block';
     });
 });
+
 
 
 
