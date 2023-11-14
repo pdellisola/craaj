5960c07c60cc9bdc2acae3f3fca3c4c216693ad1
pdellisola <150759224+pdellisola@users.noreply.github.com>
2023-11-13 23:43:06 -0300

    Update craajhead.js

--- craajhead.js
+++ craajhead.js
@@ -1,36 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Select the menu item that should be hidden when logged in
+    // Hide the menu item if the user is logged in
     var menuItemToHide = document.querySelector('span.dropdown__item a[href="https://www.craaj.com/registration-form"]');
-
-    // Check if the user avatar is present, which indicates a user is logged in
     var userAvatar = document.querySelector('.user__avatar');
+    if (userAvatar && menuItemToHide) {
+        menuItemToHide.parentElement.style.display = 'none';
+    }
 
-    // If the user is logged in, hide the menu item
-    if (userAvatar) {
-        if (menuItemToHide) {
-            menuItemToHide.parentElement.style.display = 'none';
+    // Initialize the forms based on the toggle state
+    var coachToggle = document.getElementById('coachToggle');
+    var coachForm = document.getElementById('block-1699916884056');
+    var nonCoachForm = document.getElementById('block-1699916941256');
+
+    // Set initial visibility of forms (non-coach form visible, coach form hidden)
+    coachForm.style.display = 'none';
+    nonCoachForm.style.display = 'block';
+
+    // Add event listener to the toggle switch
+    coachToggle.addEventListener('change', function() {
+        if (this.checked) {
+            // If toggle is set to 'I'm a coach'
+            coachForm.style.display = 'block';
+            nonCoachForm.style.display = 'none';
+        } else {
+            // If toggle is set to 'I'm not a coach'
+            coachForm.style.display = 'none';
+            nonCoachForm.style.display = 'block';
         }
-    }
+    });
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-  var coachToggle = document.getElementById('coachToggle');
-  var coachForm = document.getElementById('block-1699916884056');
-  var nonCoachForm = document.getElementById('block-1699916941256');
-
-  // Set the default state of the toggle switch
-  coachToggle.checked = false;
-
-  coachToggle.addEventListener('change', function() {
-    if (this.checked) {
-      // If toggle is set to 'I'm not a coach'
-      coachForm.style.display = 'none';
-      nonCoachForm.style.display = 'block';
-    } else {
-      // If toggle is set to 'I'm a coach'
-      coachForm.style.display = 'block';
-      nonCoachForm.style.display = 'none';
-    }
-  });
-});
-
